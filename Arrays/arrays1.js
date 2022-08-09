15a9d604c80e80d1c29157497e81ae42f7dc5623
Shailesh Tekale <99394274+shaileshtekale@users.noreply.github.com>
2022-08-09 13:00:02 +0530

    arrays

---
+++ Arrays/arrays1.js
@@ -0,0 +1,26 @@
+let num_array = [1, 2, 8, 4, 10, 6, 7, 3, 9, 5];
+
+// let sqr_array = [];
+// let num_square = function (num) {
+//     sqr_array.push(num * num);
+// };
+// num_array.forEach(num_square);
+// console.log(sqr_array);
+
+// let num_square_map = function (num) {
+//     return num * num;
+// };
+// console.log(num_array.map(num_square_map));
+
+// let check_if_positive = function (x) {
+//     return x % 2 == 0;
+// };
+
+// console.log(num_array.filter(check_if_positive));
+
+const find_max = function (x, y) {
+    return x > y ? x : y;
+};
+
+console.log(num_array.reduce(find_max));
+
